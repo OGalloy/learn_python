43a4d6998ebbd85d5dd242bcf508ce034e12fea8
OGalloy <56289594+OGalloy@users.noreply.github.com>
2023-05-24 19:44:23 +0500

    Update task3.py

--- seminar2/task3.py
+++ seminar2/task3.py
@@ -3,14 +3,20 @@
 #Программа должна возвращать сумму и произведение* дробей. 
 #Для проверки своего кода используйте модуль fractions.
 
-from fraction import Fraction
+from fractions import Fraction
+from decimal import *
 
 
 if __name__ == "__main__":
-    print("Эта программа для расчёта произведения и суммы дробей")
+    print("Эта программа для расчёта произведения и суммы двух дробных чисел.")
     print("Формат вводимых данных: \"a/b\"")
-    num1 = input("Введите первое число: ")
-    num2 = input("Введите второе число")
-    
+    while True:
+        num1 = input("Введите первое число: ")
+        num2 = input("Введите второе число: ")
+        summary = Fraction(num1) + Fraction(num2)
+        multiply = Fraction(num1) * Fraction(num2)
+        print("Результаты вычислений:")
+        print(f"{num1} + {num2} = {summary}")
+        print(f"{num1} * {num2} = {multiply}")
 
 
