790c5b548431fdac72751a60a8945287682d38ca
OGalloy <56289594+OGalloy@users.noreply.github.com>
2023-06-18 18:23:43 +0500

    Add files via upload

--- seminar7/__init__.py
+++ seminar7/__init__.py
@@ -1,6 +1,7 @@
 from task1 import make_files
 from two_files_in_one import two_files_in_one
-from fill_names import feel_numbers
+from fill_names import fil_numbers
 from name_gen import name_gen
+from group_rename import rename
 
-__all__ = ['make_files', 'two_files_in_one', 'feel_numbers', 'name_gen', 'group_rename']
+__all__ = ['make_files', 'two_files_in_one', 'fill_numbers', 'name_gen', 'group_rename']
--- seminar7/fill_names.py
+++ seminar7/fill_names.py
@@ -5,7 +5,7 @@ MIN_NUM = -1000
 MAX_NUM = 1000
 
 
-def fill_numbers(count: int, file_name: str | Path) -> None:
+def feel_numbers(count: int, file_name: str | Path) -> None:
     with open(file_name, 'a', encoding='utf-8') as f:
         for _ in range(count):
             f.write(f'{randint(MIN_NUM, MAX_NUM)}|{uniform(MIN_NUM, MAX_NUM)}\n')
